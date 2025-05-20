e124149b0f82e06da446976e9c70b062a4aadb24
Tanguy Vallot <tanguyvallot39@gmail.com>
2025-05-20 21:30:57 +0700

    delete folders

--- programming/version.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'qtype_programming';
-$plugin->version = 2025042900;
-$plugin->requires = 2022041900;
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = 'v1.0';
-
