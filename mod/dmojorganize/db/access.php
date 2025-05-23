1baecf65bcf3dce6521a833b452094016da21376
Tran Thuc Minh Tri <minhtri170603@gmail.com>
2025-05-23 21:11:50 +0700

    Link to DMOJ organization button, no page content yet

---
+++ mod/dmojorganize/db/access.php
@@ -0,0 +1,23 @@
+<?php
+$capabilities = [
+    'mod/dmojorganize:addinstance' => [
+        'riskbitmask' => RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+    'mod/dmojorganize:view' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => [
+            'guest' => CAP_ALLOW,
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+    ],
+];
