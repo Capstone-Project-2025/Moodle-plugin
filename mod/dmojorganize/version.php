44c20c5ab5e53c0f39573a4d5a938cafd7edf3ac
Tran Thuc Minh Tri <minhtri170603@gmail.com>
2025-07-09 16:41:03 +0700

    Events and observer (unsuccessful implementation)
    I tried to do these:
    - When a new participant is added to the Moodle course linked to a DMOJ organization, that participant is automatically added to DMOJ organization as well, using the same process described above when a new organization is linked.
    - When an existing participant is removed from a linked Moodle course, that participant is automatically removed from the DMOJ organization as well (but not deleting the whole account).
    I wrote the files db/events.php and classes/observer.php based on the following reference sources:
    - This ChatGPT conversation: https://chatgpt.com/share/685020fe-38cc-8003-9a54-a7e9cbaf4d10
    - This Moodle forum discussion: https://moodle.org/mod/forum/discuss.php?d=452045
    - Official documentation: https://docs.moodle.org/dev/Events_API
    But nothing changes, the expected behavior listed above didn't happen. I'll try a different method to do the above requirements.

--- mod/dmojorganize/version.php
+++ mod/dmojorganize/version.php
@@ -5,4 +5,4 @@ $plugin->component = 'mod_dmojorganize';
 $plugin->version = 2025040201;
 $plugin->requires = 2020110900;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.0';
+$plugin->release = 'v1.1';
