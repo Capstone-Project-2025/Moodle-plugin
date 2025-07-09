d4d8c0dd3bca1e32e6fc549a59b52c951d0a61fd
Tanguy Vallot <tanguyvallot39@gmail.com>
2025-07-09 09:54:43 +0700

    clean repository

--- mod/programmingquizz/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'mod_programmingquizz';
-$plugin->version = 2025041100;
-$plugin->requires = 2022041900; // version minimale de Moodle
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = 'v0.1';
---
+++ report/programming_classmanagement/db/access.php
@@ -0,0 +1,14 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    'report/programming_classmanagement:view' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ]
+    ],
+];
