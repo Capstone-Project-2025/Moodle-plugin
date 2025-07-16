96fe852308d3ad64038805e794182113eacc5e47
Tran Thuc Minh Tri <minhtri170603@gmail.com>
2025-07-16 20:49:44 +0700

    Sync task (doesn't work)

--- classes/observer.php
+++ classes/observer.php
@@ -5,7 +5,7 @@ defined('MOODLE_INTERNAL') || die();
 
 class observer {
 
-    public static function enrol_instance_created(\core\event\enrol_instance_created $event) {
+    public static function user_enrolment_created(\core\event\user_enrolment_created $event) {
         $data = $event->get_data();
 
         $userid = $data['relateduserid']; // the user who was added
@@ -18,5 +18,6 @@ class observer {
     private static function do_something_when_user_added(int $userid, int $courseid): void {
         // Your logic here
         debugging("User $userid added to course $courseid", DEBUG_DEVELOPER);
+        error_log('A new user was enrolled!');
     }
 }
--- db/events.php
+++ db/events.php
@@ -1,13 +1,13 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$observers = array(
-    array(
-        'eventname' => '\core\event\enrol_instance_created',
-        'callback'  => 'mod_dmojorganize_observer::enrol_instance_created',
+$observers = [
+    [
+        'eventname' => '\core\event\user_enrolment_created',
+        'callback'  => 'mod_dmojorganize_observer::user_enrolment_created',
+        'includefile' => '/mod/dmojorganize/classes/observer.php',
         'priority'  => 9999,
         'internal'  => false,
-    ),
-);
-
+    ],
+];
 ?>
