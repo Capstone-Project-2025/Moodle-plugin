4593c89dfff36ea490c8bb315910e603251c262f
Ngô Hà Định <91779022+BruhGold@users.noreply.github.com>
2025-08-04 14:08:46 +0700

    Update events.php

--- local/prog/db/events.php
+++ local/prog/db/events.php
@@ -1,19 +1,19 @@
 <?php
 $observers = [
     [
-        'eventname' => '\local_myplugin\event\before_setting_updated',
-        'callback'  => 'local_myplugin_observer::unlink_dmoj',
+        'eventname' => '\local_programming\event\before_setting_updated',
+        'callback'  => 'local_programming_observer::unlink_dmoj',
     ],
     [
-        'eventname' => '\local_myplugin\event\after_setting_updated',
-        'callback'  => 'local_myplugin_observer::link_dmoj',
+        'eventname' => '\local_programming\event\after_setting_updated',
+        'callback'  => 'local_programming_observer::link_dmoj',
     ],
     [
         'eventname' => 'core\event\user_created',
-        'callback'  => 'local_myplugin_observer::link_dmoj',
+        'callback'  => 'local_programming_observer::link_dmoj',
     ],
     [
         'eventname' => 'core\event\user_deleted',
-        'callback'  => 'local_myplugin_observer::unlink_dmoj',
+        'callback'  => 'local_programming_observer::unlink_dmoj',
     ]
 ];
