d2fc7491f78a84f13b29a371851fb65ca63147b6
Ngô Hà Định <91779022+BruhGold@users.noreply.github.com>
2025-08-27 02:38:36 +0700

    Update version.php

--- question/type/programming/version.php
+++ question/type/programming/version.php
@@ -2,11 +2,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_programming';
-$plugin->version = 2025070209;
+$plugin->version = 20250702010;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'v1.0';
 $plugin->dependencies = [
-    //'local_dmoj_user_link' => ANY_VERSION,
+    'local_dmoj_user_link' => ANY_VERSION,
 ];
 
